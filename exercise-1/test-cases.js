0b2461b582d0614fb5a1d758b9ae26709541116a
Andrico <andrico@animaapp.com>
2023-10-02 20:42:54 +0100

    update test case

--- exercise-1/test-cases.js
+++ exercise-1/test-cases.js
@@ -5,14 +5,19 @@ const test1El = document.getElementById('test-1');
 
 test1El.test(() => {
   const template = document.getElementById('album-art-template');
-  const hasAlbumNameSlot = Boolean(template.querySelector('slot[name="album-name"]'))
-  return hasAlbumNameSlot
+  
+  const hasAlbumNameSlot = Boolean(template.content.querySelector('slot[name="album-name"]'))
+  const hasAlbumNameContent = Boolean(document.querySelector('[slot="album-name"]'))
+  
+  return hasAlbumNameSlot && hasAlbumNameContent
 })
 
 const test2El = document.getElementById('test-2');
 
 test2El.test(() => {
   const template = document.getElementById('album-art-template');
-  const hasArtistNameSlot = template.querySelector('slot[name="artist-name"]');
-  return hasArtistNameSlot
+  const hasArtistNameSlot = template.content.querySelector('slot[name="artist-name"]');
+  const hasArtistNameContent = Boolean(document.querySelector('[slot="artist-name"]'))
+
+  return hasArtistNameSlot && hasArtistNameContent
 })
